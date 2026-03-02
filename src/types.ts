1d086bc4e385aeb0d194725191069eba6df56ca7
prem.csaba@students.jedlik.eu <prem.csaba@students.jedlik.eugit config --global user.name csabaprem2003git config --global user.email prem.csaba@students.jedlik.eu>
2026-03-02 22:06:21 +0100

    tailwindcss és típusok

---
+++ src/types.ts
@@ -0,0 +1,41 @@
+// Az interfészek tipusa az adatbázishoz
+export interface User {
+    id: number,
+    name: string,
+    registered_at: Date,
+    password: string,
+}
+
+export interface Publish {
+    id: number,
+    name: string,
+    headquarters: string | null,  
+    is_active: boolean,
+}
+
+export interface Game {
+    id: number,
+    name: string,
+    release_year: number,
+    genre: string,
+    publisher_id: number, 
+    platforms: Array<string>, // a string[] tömbök ugyanaz
+    cover: string | null,
+    freetogame_url: string | null,
+    created_at: Date, // lehet a string,ha Date a dátum és az idő (időzóna)
+    updated_at: Date,
+}
+
+export interface Favorite {
+    user_id: number,
+    game_id: number,
+}
+
+export interface Collectible {
+    id: number,
+    game_id: number,
+    type: string,
+    description: number,
+    images: Array<string>,
+    map_location: Array<string>,
+}
---
+++ tailwindcss.config.ts
@@ -0,0 +1,11 @@
+// A tailwindcss-ben konfigurálás 
+
+import type { Config } from 'tailwindcss'
+
+module.exports = {
+    content: ['./index.html', 'src/**/*.{vue,,js,ts,jsx,tsx}'],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+} satisfies Config
