4b0beacca0ceb781ce7d92b7a061db30cb5125e8
Csabaprem03 <prem.csaba@students.jedlik.eu>
2026-04-13 10:48:30 +0200

    Docker konténerizáció hozzáadva futtatott

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,11 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
+  server:{
+    host: true,
+    port:5173,
+    wathc:{
+      usePolling:true,
+    }
+  }
 });
