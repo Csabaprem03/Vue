c2b09d2375b67bc1e8f11cdd4ad2d1d029d09b85
prem.csaba@students.jedlik.eu <prem.csaba@students.jedlik.eugit config --global user.name csabaprem2003git config --global user.email prem.csaba@students.jedlik.eu>
2026-04-19 19:09:12 +0200

    Axios az url modósított

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,9 +1,11 @@
 // Axios-t létrehozza
 import axios from "axios";
 
+const baseUrl: string = import.meta.env.VITE_API_ENDPOINT || 'http://localhost:8000/api'
+
 // környezeten hozzáadva: a Laravel backenden 8000 porton, féjléc (header): tartalom, fogadás
 const instance = axios.create({
-  baseURL: `${import.meta.env.VITE_API_ENDPOINT}/`,
+  baseURL: `${baseUrl}/`,
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
